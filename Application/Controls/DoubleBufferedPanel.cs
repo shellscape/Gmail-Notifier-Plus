dfeec7538274267f2fc8a96084cf7ffec4a2c06e
Andrew Powell <andrew@shellscape.org>
2011-01-31 17:39:32 -0500

    double buffering action ftw

---
+++ Application/Controls/DoubleBufferedPanel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GmailNotifierPlus.Controls {
+	public class DoubleBufferPanel : Panel {
+
+		public DoubleBufferPanel() {
+			// Set the value of the double-buffering style bits to true.
+			this.SetStyle(ControlStyles.DoubleBuffer | ControlStyles.UserPaint | ControlStyles.AllPaintingInWmPaint, true);
+			this.UpdateStyles();
+		}
+
+	}
+}
