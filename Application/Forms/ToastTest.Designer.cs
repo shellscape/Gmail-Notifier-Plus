856cc5238a66f61a8986abe56ec7b9aecac85d66
Andrew Powell <andrew@shellscape.org>
2011-03-23 17:40:20 -0400

    toast testing created ToastManager

--- Application/Forms/ToastTest.Designer.cs
+++ Application/Forms/ToastTest.Designer.cs
@@ -23,11 +23,33 @@
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this.components = new System.ComponentModel.Container();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 12);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(201, 30);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Pop";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// ToastTest
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(225, 52);
+			this.Controls.Add(this.button1);
+			this.Name = "ToastTest";
 			this.Text = "ToastTest";
+			this.ResumeLayout(false);
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.Button button1;
 	}
 }
--- Application/Forms/ToastTest.cs
+++ Application/Forms/ToastTest.cs
@@ -4,15 +4,78 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Windows.Forms;
+
+using System.Diagnostics;
 
 namespace GmailNotifierPlus.Forms {
 	public partial class ToastTest : Form {
 		public ToastTest() {
 			InitializeComponent();
-
-			
 		}
+
+		private void button1_Click(object sender, EventArgs e) {
+			ToastManager.Pop();
+		}
+	}
+
+	public static class ToastManager {
+
+		private static Queue<Toast> _loaf = new Queue<Toast>();
+
+		public static void Pop() {
+			Toast slice = new Toast();
+
+			slice.FormClosed += SliceClosed;
+
+			PositionSlice(slice);
+
+			_loaf.Enqueue(slice);
+
+			slice.Show();
+		}
+
+		private static void PositionSlice(Toast slice) {
+
+			Screen primary = Screen.PrimaryScreen;
+			Rectangle workingArea = primary.WorkingArea;
+			Point start = new Point(workingArea.Width - slice.Width, workingArea.Height - slice.Height);
+
+			start.Y -= (_loaf.Count * slice.Height); // all of the slices are a constant height
+
+			slice.Location = start;
+		}
+
+		private static void SliceClosed(object sender, EventArgs e) {
+
+			Toast burnt = sender as Toast;
+			_loaf.Dequeue();
+
+			foreach (Toast slice in _loaf) {
+				Slide(slice);
+			}
+		}
+
+		private static void Slide(Toast slice) {
+
+			int targetY = slice.Top + slice.Height;
+			Timer timer = new Timer() {
+				Interval = 10
+			};
+
+			timer.Tick += delegate(object sender, EventArgs args) {
+				slice.Top = Math.Min(slice.Top + 10, targetY);
+
+				if (slice.Top == targetY) {
+					timer.Stop();
+					timer.Dispose();
+				}
+			};
+
+			timer.Start();
+		}
+
 	}
 }
