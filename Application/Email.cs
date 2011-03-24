cf6b529e8c34782c6fee6519a8087ee323c27619
Andrew Powell <andrew@shellscape.org>
2011-03-24 10:50:27 -0400

    initial commit. no more xml parsing everywhere

---
+++ Application/Email.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace GmailNotifierPlus {
+	public class Email {
+
+		public Email() {
+			From = Title = Message = Date = Url = String.Empty;
+		}
+
+		public String From { get; set; }
+		public String Title { get; set; }
+		public String Message { get; set; }
+		public String Date { get; set; }
+		public String Url { get; set; }
+
+		public static Email FromNode(XmlNode node, Account account) {
+
+			DateTime time = DateTime.Parse(node.ChildNodes.Item(3).InnerText.Replace("T24:", "T00:"));
+
+			Email email = new Email() {
+				Title = string.IsNullOrEmpty(node.ChildNodes.Item(0).InnerText) ? Locale.Current.Labels.NoSubject : node.ChildNodes.Item(0).InnerText,
+				Message = node.ChildNodes.Item(1).InnerText,
+				Date = time.ToShortDateString() + " " + time.ToShortTimeString(),
+				Url = Utilities.UrlHelper.BuildMailUrl(node.ChildNodes.Item(2).Attributes["href"].Value, account)
+			};
+
+			if ((node.ChildNodes.Item(6) != null) && (node.ChildNodes.Item(6).ChildNodes.Item(1) != null)) {
+				email.From = node.ChildNodes.Item(6).ChildNodes[1].InnerText;
+			}
+
+			return email;
+		}
+	}
+}
