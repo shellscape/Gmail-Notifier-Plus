ffd0f48ecb89dcad6a0f1bd647b7fdcbfe7398da
shellscape <andrew@shellscape.org>
2011-08-27 00:10:28 -0400

    new locale schema

---
+++ Application/Forms/Prefs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Shellscape.UI.ControlPanel;
+
+namespace GmailNotifierPlus.Forms {
+	public partial class Prefs : Shellscape.UI.ControlPanel.ControlPanelForm {
+		
+		public Prefs() {
+			InitializeComponent();
+
+			// FlowLayoutPanel doesn't support visual inheritance, so we have to add links manually here.
+		}
+
+		private void InitButtons() {
+			ControlPanelTaskLink general = new ControlPanelTaskLink() { Text = Locale.Current.Config.General };
+			ControlPanelTaskLink accounts = new ControlPanelTaskLink() { Text = Locale.Current.Config.Accounts };
+			ControlPanelTaskLink appearance = new ControlPanelTaskLink() { Text = Locale.Current.Config.Appearance };
+
+			_ButtonNewAccount.Text = Locale.Current.Buttons.AddNewAccount;
+			_ButtonGeneral.ButtonText = ;
+			_ButtonAccounts.ButtonText = ;
+			_ButtonAppearance.ButtonText = ;
+
+			this._Tasks.Controls.Add(general);
+		}
+
+		protected override void OnLoad(EventArgs e) {
+			base.OnLoad(e);
+
+		}
+	}
+}
---
+++ Application/Localization/Locale.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Linq;
+using System.Xml.Serialization;
+
+namespace GmailNotifierPlus.Localization {
+
+	[XmlType(AnonymousType = true)]
+	[XmlRoot(ElementName = "Locale", Namespace = "", IsNullable = false)]
+	public class Locale {
+
+		private String[] RtlLangs = new String[] { "ar-dz", "he-il", "fa-ir" };
+
+		[XmlIgnore]
+		public String LanguageCode { get; set; }
+
+		[XmlIgnore]
+		internal bool IsRightToLeftLanguage {
+			get {
+				return RtlLangs.Contains(this.LanguageCode);
+			}
+		}
+
+		[XmlIgnore]
+		public static Locale Current { get; private set; }
+
+		public void Init() {
+			Current = this;
+		}
+
+		public String Name { get; set; }
+		public LocaleCommon Common { get; set; }
+		public LocaleJumpList JumpList { get; set; }
+		public LocaleAbout About { get; set; }
+		public LocalePreferences Preferences { get; set; }
+		public LocaleThumbnails Thumbnails { get; set; }
+		public LocaleToast Toast { get; set; }
+
+	}
+
+	public class LocaleCommon {
+
+		public String Cancel { get; set; }
+		public String Close { get; set; }
+		public String Next { get; set; }
+		public String OK { get; set; }
+		public String Previous { get; set; }
+		public String No { get; set; }
+		public String Yes { get; set; }
+		public String Unread { get; set; }
+
+	}
+
+	public class LocaleJumpList {
+
+		public String Inbox { get; set; }
+		public String Compose { get; set; }
+		public String Check { get; set; }
+		public String Preferences { get; set; }
+		public String About { get; set; }
+		public String Help { get; set; }
+
+	}
+
+	public class LocaleAbout {
+
+		public String WindowTitle { get; set; }
+		public String Button { get; set; }
+
+	}
+
+	public class LocalePreferences {
+
+		public String WindowTitle { get; set; }
+		public LocalePreferencesNavigation Navigation { get; set; }
+		public LocalePreferencesPanels Panels { get; set; }
+	}
+
+	public class LocalePreferencesNavigation {
+
+		public String General { get; set; }
+		public String Accounts { get; set; }
+		public String Appearance { get; set; }
+
+	}
+
+	public class LocalePreferencesPanels {
+
+		public LocaleGeneral General { get; set; }
+		public LocaleAccountPanel Accounts { get; set; }
+		public LocaleAppearance Appearance { get; set; }
+
+	}
+
+	public class LocaleGeneral {
+
+		public String Title { get; set; }
+		public String CheckEmail { get; set; }
+		public String CheckMinutes { get; set; }
+		public String CheckUpdates { get; set; }
+		public LocaleGeneralSound Sound { get; set; }
+	}
+
+	public class LocaleGeneralSound {
+
+		public String Title { get; set; }
+		public String None { get; set; }
+		public String Default { get; set; }
+		public String Custom { get; set; }
+		public String Browse { get; set; }
+		public String BrowseWindowTitle { get; set; }
+		public String WaveFiles { get; set; }
+
+	}
+
+	public class LocaleAccountPanel {
+
+		public String Title { get; set; }
+		public String AddNew { get; set; }
+		public LocaleAccount Account { get; set; }
+	}
+
+	public class LocaleAccount {
+
+		public String Address { get; set; }
+		public String Password { get; set; }
+		public String Mailto { get; set; }
+		public String DefaultAccount { get; set; }
+		public String Browser { get; set; }
+		public String AddAccount { get; set; }
+		public String RemoveAccount { get; set; }
+		public String RemoveConfirmation { get; set; }
+		public String RemoveConfirmationTitle { get; set; }
+		public String AccountsIntroNone { get; set; }
+		public String Error { get; set; }
+	}
+
+	public class LocaleAppearance {
+
+		public String Title { get; set; }
+		public String Language { get; set; }
+		public String FlashTaskbar { get; set; }
+		public String ShowTray { get; set; }
+		public String ShowToast { get; set; }
+	}
+
+	public class LocaleThumbnails {
+
+		public String CheckLogin { get; set; }
+		public String Connecting { get; set; }
+		public String ConnectionUnavailable { get; set; }
+		public String NoSubject { get; set; }
+		public String NoAccount { get; set; }
+		public String NoMail { get; set; }
+	}
+
+	public class LocaleToast {
+
+		public String OpenInbox { get; set; }
+		public String NoSubject { get; set; }
+	}
+
+}
