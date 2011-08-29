6bd86cab068fc672ffbfbef6d512f1ca1ba9f2a3
shellscape <andrew@shellscape.org>
2011-08-28 21:26:10 -0400

    added update checking animation/info

--- Application/Forms/About.Designer.cs
+++ Application/Forms/About.Designer.cs
@@ -30,13 +30,58 @@ namespace GmailNotifierPlus.Forms {
 		/// the contents of this method with the code editor.
 		/// </summary>
 		private void InitializeComponent() {
-			this._PanelAbout = new GmailNotifierPlus.Controls.AboutPanel();
-			this._PanelShellscape = new Shellscape.UI.Controls.DoubleBufferedPanel();
 			this._Panel = new System.Windows.Forms.Panel();
 			this._ButtonOK = new System.Windows.Forms.Button();
-			this._PanelAbout.SuspendLayout();
+			this._PanelUpdate = new Shellscape.UI.Controls.DoubleBufferedPanel();
+			this._LabelUpdate = new System.Windows.Forms.Label();
+			this._PanelAbout = new GmailNotifierPlus.Controls.AboutPanel();
+			this._PanelShellscape = new Shellscape.UI.Controls.DoubleBufferedPanel();
 			this._Panel.SuspendLayout();
+			this._PanelUpdate.SuspendLayout();
+			this._PanelAbout.SuspendLayout();
 			this.SuspendLayout();
+			// 
+			// _Panel
+			// 
+			this._Panel.Controls.Add(this._PanelUpdate);
+			this._Panel.Controls.Add(this._ButtonOK);
+			this._Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this._Panel.Location = new System.Drawing.Point(0, 302);
+			this._Panel.Name = "_Panel";
+			this._Panel.Padding = new System.Windows.Forms.Padding(6);
+			this._Panel.Size = new System.Drawing.Size(298, 80);
+			this._Panel.TabIndex = 2;
+			// 
+			// _ButtonOK
+			// 
+			this._ButtonOK.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this._ButtonOK.Location = new System.Drawing.Point(6, 32);
+			this._ButtonOK.Name = "_ButtonOK";
+			this._ButtonOK.Size = new System.Drawing.Size(286, 42);
+			this._ButtonOK.TabIndex = 0;
+			this._ButtonOK.Text = "button1";
+			this._ButtonOK.UseVisualStyleBackColor = true;
+			// 
+			// _PanelUpdate
+			// 
+			this._PanelUpdate.Controls.Add(this._LabelUpdate);
+			this._PanelUpdate.Dock = System.Windows.Forms.DockStyle.Fill;
+			this._PanelUpdate.Location = new System.Drawing.Point(6, 6);
+			this._PanelUpdate.Name = "_PanelUpdate";
+			this._PanelUpdate.Padding = new System.Windows.Forms.Padding(22, 0, 0, 4);
+			this._PanelUpdate.Size = new System.Drawing.Size(286, 26);
+			this._PanelUpdate.TabIndex = 2;
+			this._PanelUpdate.Paint += new System.Windows.Forms.PaintEventHandler(this._PanelUpdate_Paint);
+			// 
+			// _LabelUpdate
+			// 
+			this._LabelUpdate.Dock = System.Windows.Forms.DockStyle.Fill;
+			this._LabelUpdate.Location = new System.Drawing.Point(22, 0);
+			this._LabelUpdate.Name = "_LabelUpdate";
+			this._LabelUpdate.Size = new System.Drawing.Size(264, 22);
+			this._LabelUpdate.TabIndex = 1;
+			this._LabelUpdate.Text = "Checking for updates...";
+			this._LabelUpdate.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
 			// 
 			// _PanelAbout
 			// 
@@ -46,7 +91,7 @@ namespace GmailNotifierPlus.Forms {
 			this._PanelAbout.Location = new System.Drawing.Point(0, 0);
 			this._PanelAbout.Margin = new System.Windows.Forms.Padding(0);
 			this._PanelAbout.Name = "_PanelAbout";
-			this._PanelAbout.Size = new System.Drawing.Size(298, 356);
+			this._PanelAbout.Size = new System.Drawing.Size(298, 382);
 			this._PanelAbout.TabIndex = 1;
 			// 
 			// _PanelShellscape
@@ -58,32 +103,12 @@ namespace GmailNotifierPlus.Forms {
 			this._PanelShellscape.Size = new System.Drawing.Size(80, 86);
 			this._PanelShellscape.TabIndex = 1;
 			// 
-			// _Panel
-			// 
-			this._Panel.Controls.Add(this._ButtonOK);
-			this._Panel.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this._Panel.Location = new System.Drawing.Point(0, 302);
-			this._Panel.Name = "_Panel";
-			this._Panel.Padding = new System.Windows.Forms.Padding(6);
-			this._Panel.Size = new System.Drawing.Size(298, 54);
-			this._Panel.TabIndex = 2;
-			// 
-			// _ButtonOK
-			// 
-			this._ButtonOK.Dock = System.Windows.Forms.DockStyle.Fill;
-			this._ButtonOK.Location = new System.Drawing.Point(6, 6);
-			this._ButtonOK.Name = "_ButtonOK";
-			this._ButtonOK.Size = new System.Drawing.Size(286, 42);
-			this._ButtonOK.TabIndex = 0;
-			this._ButtonOK.Text = "button1";
-			this._ButtonOK.UseVisualStyleBackColor = true;
-			// 
 			// About
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.BackColor = System.Drawing.SystemColors.Window;
-			this.ClientSize = new System.Drawing.Size(298, 356);
+			this.ClientSize = new System.Drawing.Size(298, 382);
 			this.Controls.Add(this._Panel);
 			this.Controls.Add(this._PanelAbout);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -92,8 +117,9 @@ namespace GmailNotifierPlus.Forms {
 			this.Name = "About";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
 			this.Text = "Gmail Notifier Plus";
-			this._PanelAbout.ResumeLayout(false);
 			this._Panel.ResumeLayout(false);
+			this._PanelUpdate.ResumeLayout(false);
+			this._PanelAbout.ResumeLayout(false);
 			this.ResumeLayout(false);
 
 		}
@@ -104,6 +130,8 @@ namespace GmailNotifierPlus.Forms {
 		private Shellscape.UI.Controls.DoubleBufferedPanel _PanelShellscape;
 		private Panel _Panel;
 		private Button _ButtonOK;
+		private Label _LabelUpdate;
+		private Shellscape.UI.Controls.DoubleBufferedPanel _PanelUpdate;
 
 	}
 }
--- Application/Forms/About.cs
+++ Application/Forms/About.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Reflection;
@@ -15,25 +16,75 @@ using Microsoft.WindowsAPI.Shell;
 using GmailNotifierPlus.Controls;
 using GmailNotifierPlus.Utilities;
 
+using Shellscape;
+
 namespace GmailNotifierPlus.Forms {
 	public partial class About : Form {
 
+		public enum UpdateStatus {
+			Checking,
+			UpToDate,
+			Problem
+		}
+
+		private AnimatedCursor _animatedCursor;
+		private Bitmap _upToDate;
+		private Bitmap _exclamation;
 		private Boolean _firstRun;
+
+		private UpdateStatus _status;
+
+		private Timer _timer;
 
 		[System.Runtime.InteropServices.DllImport("user32.dll")]
 		public static extern bool SetForegroundWindow(IntPtr hWnd);
-		
+
 		public About() {
 			InitializeComponent();
 
+			this.TopMost = true;
 			this.Icon = Program.Icon;
-			//this.ClientSize = new Size(300, 355);
+
 			this.SetStyle(ControlStyles.AllPaintingInWmPaint | ControlStyles.UserPaint | ControlStyles.DoubleBuffer, true);
 			this.Text = Resources.WindowTitle;
 
 			_PanelAbout.FirstRun = _firstRun = Config.Current.FirstRun;
 			_ButtonOK.Click += _ButtonOk_Click;
 			_ButtonOK.Text = _firstRun ? Localization.Locale.Current.About.FirstRun : Localization.Locale.Current.About.Button;
+
+			_upToDate = Utilities.ResourceHelper.GetImage("uptodate.png");
+			_exclamation = Utilities.ResourceHelper.GetImage("exclamation-updates.png");
+
+			_animatedCursor = new AnimatedCursor(Cursors.WaitCursor);
+			_status = UpdateStatus.Checking;
+
+			_timer = new Timer() { Interval = 75 };
+
+			_timer.Tick += delegate(object sender, EventArgs e) {
+				_PanelUpdate.Invalidate(new Rectangle(0, 0, 26, 26));
+
+				if (_status != UpdateStatus.Checking) {
+					_timer.Stop();
+				}
+			};
+
+			_timer.Start();
+
+			Random random = new Random();
+			Timer temp = new Timer() { Interval = random.Next(5000, 10000) };
+
+			// this is just temporary until the code gets written to check github.
+			temp.Tick += delegate(object sender, EventArgs e) {
+				
+				String version = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+				_status = UpdateStatus.UpToDate;
+				_LabelUpdate.Text = String.Concat("Gmail Notifier Plus is up to date (", version, ")");
+
+				temp.Stop();
+			};
+
+			temp.Start();
 		}
 
 		private void _PanelShellscape_Click(object sender, EventArgs e) {
@@ -42,7 +93,10 @@ namespace GmailNotifierPlus.Forms {
 
 		protected override void OnShown(EventArgs e) {
 			base.OnShown(e);
+
+			this.TopMost = false;
 			SetForegroundWindow(base.Handle);
+
 		}
 
 		private void _ButtonOk_Click(object sender, EventArgs e) {
@@ -53,6 +107,20 @@ namespace GmailNotifierPlus.Forms {
 			}
 
 			Close();
+		}
+
+		private void _PanelUpdate_Paint(object sender, PaintEventArgs e) {
+
+			if (_status == UpdateStatus.Checking) {
+				_animatedCursor.DrawStep(e.Graphics, -4, -4);
+			}
+			else if (_status == UpdateStatus.Problem) {
+				e.Graphics.DrawImage(_exclamation, 4, 4, _exclamation.Width, _exclamation.Height);
+			}
+			else if (_status == UpdateStatus.UpToDate) {
+				e.Graphics.DrawImage(_upToDate, 4, 4, _upToDate.Width, _upToDate.Height);
+			}
+
 		}
 	}
 }
